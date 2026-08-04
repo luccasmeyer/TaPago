c95213a7e7b6e19df6bcbd57835f3dceaa2c85d0
Lucas Alfonso Meyer <lucasmeyer.scs@gmail.com>
2026-08-04 09:03:11 -0300

    *Style* Padronização de strings do aplicativo

--- app/src/main/java/com/example/tapago/data/mapper/ExerciseMapper.kt
+++ app/src/main/java/com/example/tapago/data/mapper/ExerciseMapper.kt
@@ -6,7 +6,7 @@ import com.example.tapago.domain.model.Exercise
 fun ExercisesEntity.toDomain(): Exercise {
     return Exercise(
         idExercise = this.exerciseId,
-        nameExercise = this.nameExercise,
+        nameExercise = this.nameExercise.replaceFirstChar { it.uppercase() }.trim(),
         typeExercise = this.typeExrcise
     )
 }
